055fdc4aa0248996ebfb1c65a0b5a714262c4005
brandmch <101467867+brandmch@users.noreply.github.com>
2022-05-15 17:23:17 -0500

    minor

--- src/Components/about_me.js
+++ src/Components/about_me.js
@@ -4,13 +4,16 @@ export default function About_Me() {
     return (
 
         <div className="resume">
-            <div className="resume-links">
-                <a href="https://www.linkedin.com/in/mchugh-brandon/" target="_blank">LinkedIn</a>
-                <a href="https://github.com/brandmch" target="_blank">GitHub</a>
-                <a href="/">Home</a>
+            <div>
+                <div>
+                    <div className="resume-links">
+                        <a href="https://www.linkedin.com/in/mchugh-brandon/" target="_blank">LinkedIn</a>
+                        <a href="https://github.com/brandmch" target="_blank">GitHub</a>
+                        <a href="/">Home</a>
+                    </div>
+                    <h1 className="resume-header">Brandon Lloyd McHugh</h1>
+                </div>
             </div>
-            <h1>Brandon Lloyd McHugh</h1>
-
             <div>
                 <div>
                     <div className="resume-sections">
