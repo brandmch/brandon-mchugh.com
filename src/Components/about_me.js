dd14b27f6eebd6750491f6fbebe4e6f2b8bbf58d
brandmch <101467867+brandmch@users.noreply.github.com>
2022-05-15 17:21:42 -0500

    minor

--- src/Components/about_me.js
+++ src/Components/about_me.js
@@ -9,6 +9,18 @@ export default function About_Me() {
                 <a href="https://github.com/brandmch" target="_blank">GitHub</a>
                 <a href="/">Home</a>
             </div>
+            <h1>Brandon Lloyd McHugh</h1>
+
+            <div>
+                <div>
+                    <div className="resume-sections">
+                        <h3>Skills</h3>
+                        <h3>Projects</h3>
+                        <h3>Work Experience</h3>
+                        <h3>Education</h3>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
