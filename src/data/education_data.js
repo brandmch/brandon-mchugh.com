7ee96115436add764f58698e19399e68051023f5
brandmch <101467867+brandmch@users.noreply.github.com>
2022-05-16 16:41:48 -0500

    all info and basic css done

---
+++ src/data/education_data.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default [
+    {
+        "organization": "Rowan University",
+        "degree": "Bachelor of Science",
+        "discipline": "Psychological Sciences"
+    }
+]
