63f142616f7d1127ff54d020d1ce27d5e2c9f041
brandmch <101467867+brandmch@users.noreply.github.com>
2022-05-16 16:02:14 -0500

    Work experience css done

--- src/Components/About_Me/work_experience.js
+++ src/Components/About_Me/work_experience.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Work_Experience_Container from './work_experience_container';
+import data from '../../data/work_experience_data';
+
+const work_experience = data.map(x => {
+    return <Work_Experience_Container
+        key={x.id}
+        {...x} />
+})
+
 
 export default function Work_Experience() {
     return (
         <div className='work_experience'>
             <h1 className='sections_h1'>Work Experience</h1>
+            {work_experience}
         </div>
     )
 }
