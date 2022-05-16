de47bd9c0d86713fb739120131d92d48b30f2263
brandmch <101467867+brandmch@users.noreply.github.com>
2022-05-16 00:15:00 -0500

    Skills entered

--- src/Components/About_Me/skills.js
+++ src/Components/About_Me/skills.js
@@ -4,6 +4,23 @@ export default function Skills() {
     return (
         <div className='skills'>
             <h1 className='sections_h1'>Skills</h1>
+            <div class="grid-container">
+                <div class="grid-item">Java</div>
+                <div class="grid-item">JavaScript</div>
+                <div class="grid-item">SQL</div>
+                <div class="grid-item">SQLite</div>
+                <div class="grid-item">xml</div>
+                <div class="grid-item">React</div>
+                <div class="grid-item">Node.js</div>
+                <div class="grid-item">Express.js</div>
+                <div class="grid-item">MongoDB</div>
+                <div class="grid-item">JSON</div>
+                <div class="grid-item">Github</div>
+                <div class="grid-item">Android Studio</div>
+                <div class="grid-item">Visual Studio Code</div>
+                <div class="grid-item">Google Play Console</div>
+
+            </div>
         </div>
     )
 }
