ba1d6dbf33305c5f0c551ac796789b5bca2ad945
brandmch <101467867+brandmch@users.noreply.github.com>
2022-05-15 21:09:20 -0500

    resume functionality done

---
+++ src/Components/About_Me/about_me.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function About_Me() {
+    return (
+
+        <div>
+            <header>
+                <div class="container container_solid resume">
+                    <div class="title_wrapper">
+                        <div className="links">
+                            <a href="https://www.linkedin.com/in/mchugh-brandon/" target="_blank">LinkedIn</a>
+                            <a href="https://github.com/brandmch" target="_blank">GitHub</a>
+                            <a href="/">Home</a>
+                        </div>
+                        <h1>Brandon Lloyd McHugh</h1>
+                    </div>
+                </div>
+            </header>
+
+            <div className='section'>
+                <div>
+                    <div>
+                        <h3>Skills</h3>
+                        <h3>Projects</h3>
+                        <h3>Work Experience</h3>
+                        <h3>Education</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import Maintenance_Alert from './Maintence_Alert'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import About_Me from './Components/about_me';
+import About_Me from './Components/About_Me/about_me';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
