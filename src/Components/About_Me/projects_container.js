809aefc89f017516a79274e596cbf093084f4c13
brandmch <101467867+brandmch@users.noreply.github.com>
2022-05-16 15:17:37 -0500

    skills, projects, data added and mapped

--- src/Components/About_Me/projects_container.js
+++ src/Components/About_Me/projects_container.js
@@ -5,10 +5,12 @@ export default function Project_Container(props) {
     return (
         <div className="projectContainer">
             <h1>{props.projectName}</h1>
-            <a href={props.github} target="_blank" rel="noreferrer">GitHub</a>
-            <p>{props.organization}</p>
-            <p>{props.date}</p>
-            <p>{props.description}</p>
+            <div className="info">
+                <a href={props.github} target="_blank" rel="noreferrer">GitHub</a>
+                <p>Organization: {props.organization}</p>
+                <p>{props.date}</p>
+                <p>{props.description}</p>
+            </div>
         </div>
     )
 }
--- src/Components/About_Me/skills.js
+++ src/Components/About_Me/skills.js
@@ -1,24 +1,19 @@
 import React from 'react';
+import data from '../../data.js/skills_data'
+
+const skills = data.map(x => {
+    return <div
+        class="grid-item"
+    >{x}</div>
+})
 
 export default function Skills() {
     return (
         <div className='skills'>
             <h1 className='sections_h1'>Skills</h1>
             <div class="grid-container">
-                <div class="grid-item">Java</div>
-                <div class="grid-item">JavaScript</div>
-                <div class="grid-item">SQL</div>
-                <div class="grid-item">SQLite</div>
-                <div class="grid-item">xml</div>
-                <div class="grid-item">React</div>
-                <div class="grid-item">Node.js</div>
-                <div class="grid-item">Express.js</div>
-                <div class="grid-item">MongoDB</div>
-                <div class="grid-item">JSON</div>
-                <div class="grid-item">Github</div>
-                <div class="grid-item">Android Studio</div>
-                <div class="grid-item">Visual Studio Code</div>
-                <div class="grid-item">Google Play Console</div>
+
+                {skills}
 
             </div>
         </div>
