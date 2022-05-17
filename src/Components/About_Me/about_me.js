548e5ffd123fdec2e219af6b42c5a886909820ba
brandmch <101467867+brandmch@users.noreply.github.com>
2022-05-17 12:31:39 -0500

    testing changing background

--- src/Components/About_Me/about_me.js
+++ src/Components/About_Me/about_me.js
@@ -4,21 +4,35 @@ import Projects from "./projects";
 import Skills from "./skills";
 import Work_Experience from "./work_experience";
 import MaintenanceAlert from '../../Maintence_Alert';
+import pics from '../../Images/images'
+
+// "../../Images/arches.JPG"
 
 
 export default function About_Me() {
+
+    let [bgpic, setBgpic] = React.useState();
+    bgpic = document.getElementById('bg_img').style.backgroundImage;
+
+    function changeBackground() {
+        setBgpic(pics.pic10);
+        console.log("Works ");
+
+    }
+
     return (
 
         <div>
             <header>
-                <div class="container container_solid resume container_image">
+                <div class="container container_solid resume container_image" id="bg_img">
                     <div class="title_wrapper">
                         <div className="links">
                             <a href="https://www.linkedin.com/in/mchugh-brandon/" target="_blank" rel="noreferrer">LinkedIn</a>
                             <a href="https://github.com/brandmch" target="_blank" rel="noreferrer">GitHub</a>
                             <a href="/home">Home</a>
                         </div>
-                        <h1>Brandon Lloyd McHugh</h1>
+                        <h1 id="h1">Brandon Lloyd McHugh</h1>
+                        <button type="button" onClick={changeBackground}>HEY</button>
                     </div>
                 </div>
             </header>
@@ -30,13 +44,6 @@ export default function About_Me() {
                 <Education />
             </div>
 
-            {/* <div className="text-align">
-                <div className="footer">
-                    <a href="https://www.linkedin.com/in/mchugh-brandon/" target="_blank">LinkedIn</a>
-                    <a href="https://github.com/brandmch" target="_blank">GitHub</a>
-                    <a href="/">Home</a>
-                </div>
-            </div> */}
         </div>
     )
 }
