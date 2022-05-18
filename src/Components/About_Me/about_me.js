52612cf7e4b731e7c7aa61e2c2d947749482ecc6
brandmch <101467867+brandmch@users.noreply.github.com>
2022-05-18 00:36:21 -0500

    Pictures moved to public

--- src/Components/About_Me/about_me.js
+++ src/Components/About_Me/about_me.js
@@ -4,35 +4,35 @@ import Projects from "./projects";
 import Skills from "./skills";
 import Work_Experience from "./work_experience";
 import MaintenanceAlert from '../../Maintence_Alert';
-import pics from '../../Images/images'
+
+
 
 // "../../Images/arches.JPG"
 
 
 export default function About_Me() {
 
-    let [bgpic, setBgpic] = React.useState();
-    bgpic = document.getElementById('bg_img').style.backgroundImage;
+    const urll = "Images/pic1.JPG"
 
-    function changeBackground() {
-        setBgpic(pics.pic10);
-        console.log("Works ");
+    const bgimg = {
+        backgroundImage: `URL(${urll})`
 
     }
+    // document.getElementById(bgimg).style.backgroundImage
 
     return (
 
         <div>
             <header>
-                <div class="container container_solid resume container_image" id="bg_img">
-                    <div class="title_wrapper">
+                <div style={bgimg} className="container container_solid resume container_image" id="bg_img">
+                    <div className="title_wrapper">
                         <div className="links">
                             <a href="https://www.linkedin.com/in/mchugh-brandon/" target="_blank" rel="noreferrer">LinkedIn</a>
                             <a href="https://github.com/brandmch" target="_blank" rel="noreferrer">GitHub</a>
                             <a href="/home">Home</a>
                         </div>
                         <h1 id="h1">Brandon Lloyd McHugh</h1>
-                        <button type="button" onClick={changeBackground}>HEY</button>
+                        <button type="button">HEY</button>
                     </div>
                 </div>
             </header>
--- src/Images/images.js
+++ src/Images/images.js
@@ -1,22 +1,5 @@
 import React from "react";
 import savannahPic from './savannah.jpg';
-import pic1 from './yellow_stone_copy.JPG'
-import pic2 from './arches.JPG'
-import pic3 from './cape_hatteras.JPG'
-import pic4 from './grand_teton.JPG'
-import pic5 from './red_rocks.JPG'
-import pic6 from './IMG_0025.JPG'
-import pic7 from './IMG_0177.JPG'
-import pic8 from './IMG_6573.JPG'
-import pic9 from './IMG_7332.JPG'
-import pic10 from './IMG_7649.JPG'
-import pic11 from './IMG_7686.JPG'
-import pic12 from './IMG_8634.JPG'
-import pic14 from './IMG_8820.JPG'
-import pic15 from './IMG_9054.JPG'
-import pic16 from './IMG_9780.JPG'
-import pic17 from './IMG_9810.JPG'
 
 
-
-export default { savannahPic, pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8, pic9, pic10, pic11, pic12, pic14, pic15, pic16, pic17 };
+export default { savannahPic };
