29941ab2774e17580dedeccb4da6634f82f328ea
brandmch <101467867+brandmch@users.noreply.github.com>
2022-05-21 12:58:21 -0500

    Updated work experience

--- src/data/work_experience_data.js
+++ src/data/work_experience_data.js
@@ -1,53 +1,66 @@
-import React from 'react';
+import React from "react";
 
 export default [
-    {
-        "id": 1,
-        "job_title": "Sales Development Representative",
-        "company": "Procore Technologies",
-        "dates": "Nov 2021 - Feb 2022",
-        "description": "Creating new business opportunities to fuel Procore’s growth. Engaging construction professionals through phone, email, and other channels to understand their challenges and identify opportunities to solve them. Partnering with Account Executives to achieve mutually aligned sales quotas each month. Autonomously operating beyond the role to help the team improve processes, training, use of technology, and any other aspects of business. Working with sales management to develop targeted lists, call strategies, and messaging to create opportunities for new business."
-    },
-    {
-        "id": 2,
-        "job_title": "Sales Development Representative",
-        "company": "Advanced Systems Concepts Inc",
-        "dates": "Mar 2020 - May 2021",
-        "description": "Pioneered Advanced Systems Concepts’ Sales Development team. Responsible for assessing, identifying and implementing novel approaches and techniques to quickly identify high potential clients for ActiveBatch Workload Automation. Daily tasks include inbound and outbound phone calls, emails, LinkedIn messages, and meeting with the SDR and Account Executive teams to discuss plans of action. By vetting new leads, I was able to drastically boost the efficiency of our sales department and increase revenue. Also responsible for nurturing current clients to discover new business opportunities."
-    },
-    {
-        "id": 3,
-        "job_title": "Fitness Sales Associate",
-        "company": "Orange Theory Fitness",
-        "dates": "Feb 2020 - Mar 2020",
-        "description": "Meeting daily sales goals by contacting prospective clients, giving detailed explanations of fitness packages and catering them to clients’ individual needs."
-    },
-    {
-        "id": 4,
-        "job_title": "Assistant",
-        "company": "KZ Landscaping",
-        "dates": "May 2019 - Dec 2019",
-        "description": "Helped grow an independent landscaping company by increasing productivity to make room for new customers, while maintaining high quality work for all customers."
-    },
-    {
-        "id": 5,
-        "job_title": "Head Lifeguard",
-        "company": "White Meadow Lake POA",
-        "dates": "May 2016 - August 2017",
-        "description": "Responsible for overseeing 45 lifeguards in five separate locations, interacting with patrons, solving conflicts, enforcing site rules and regulations, ensuring safe enjoyment of three beaches and two pools, orchestrating rescue drills, relaying information from corporate to lifeguards, and attending board meetings."
-    },
-    {
-        "id": 6,
-        "job_title": "Lifeguard",
-        "company": "White Meadow Lake POA",
-        "dates": "May 2012 - May 2016",
-        "description": "Responsible for surveying patrons in the water, enforcing rules and regulations, performing water rescues, applying first aid when needed, checking badges at front gate, and keeping sites clean. "
-    },
-    {
-        "id": 7,
-        "job_title": "Crew Member",
-        "company": "Chipotle Mexican Grill",
-        "dates": "February 2014 - June 2015",
-        "description": "Responsible for engaging customers to help maximize their dining experience, building unique meals around individual desires, maintaining high-quality product standards, grilling a variety of meats and vegetables, making different salsas, rice, and guacamole, and finalizing sales. "
-    }
-]
+  {
+    id: 1,
+    job_title: "Software Engineer",
+    company: "StealthCorp",
+    dates: "May 2022 - Present",
+  },
+  {
+    id: 2,
+    job_title: "Sales Development Representative",
+    company: "Procore Technologies",
+    dates: "Nov 2021 - Feb 2022",
+    description:
+      "Creating new business opportunities to fuel Procore’s growth. Engaging construction professionals through phone, email, and other channels to understand their challenges and identify opportunities to solve them. Partnering with Account Executives to achieve mutually aligned sales quotas each month. Autonomously operating beyond the role to help the team improve processes, training, use of technology, and any other aspects of business. Working with sales management to develop targeted lists, call strategies, and messaging to create opportunities for new business.",
+  },
+  {
+    id: 3,
+    job_title: "Sales Development Representative",
+    company: "Advanced Systems Concepts Inc",
+    dates: "Mar 2020 - May 2021",
+    description:
+      "Pioneered Advanced Systems Concepts’ Sales Development team. Responsible for assessing, identifying and implementing novel approaches and techniques to quickly identify high potential clients for ActiveBatch Workload Automation. Daily tasks include inbound and outbound phone calls, emails, LinkedIn messages, and meeting with the SDR and Account Executive teams to discuss plans of action. By vetting new leads, I was able to drastically boost the efficiency of our sales department and increase revenue. Also responsible for nurturing current clients to discover new business opportunities.",
+  },
+  {
+    id: 4,
+    job_title: "Fitness Sales Associate",
+    company: "Orange Theory Fitness",
+    dates: "Feb 2020 - Mar 2020",
+    description:
+      "Meeting daily sales goals by contacting prospective clients, giving detailed explanations of fitness packages and catering them to clients’ individual needs.",
+  },
+  {
+    id: 5,
+    job_title: "Assistant",
+    company: "KZ Landscaping",
+    dates: "May 2019 - Dec 2019",
+    description:
+      "Helped grow an independent landscaping company by increasing productivity to make room for new customers, while maintaining high quality work for all customers.",
+  },
+  {
+    id: 6,
+    job_title: "Head Lifeguard",
+    company: "White Meadow Lake POA",
+    dates: "May 2016 - August 2017",
+    description:
+      "Responsible for overseeing 45 lifeguards in five separate locations, interacting with patrons, solving conflicts, enforcing site rules and regulations, ensuring safe enjoyment of three beaches and two pools, orchestrating rescue drills, relaying information from corporate to lifeguards, and attending board meetings.",
+  },
+  {
+    id: 7,
+    job_title: "Lifeguard",
+    company: "White Meadow Lake POA",
+    dates: "May 2012 - May 2016",
+    description:
+      "Responsible for surveying patrons in the water, enforcing rules and regulations, performing water rescues, applying first aid when needed, checking badges at front gate, and keeping sites clean. ",
+  },
+  {
+    id: 8,
+    job_title: "Crew Member",
+    company: "Chipotle Mexican Grill",
+    dates: "February 2014 - June 2015",
+    description:
+      "Responsible for engaging customers to help maximize their dining experience, building unique meals around individual desires, maintaining high-quality product standards, grilling a variety of meats and vegetables, making different salsas, rice, and guacamole, and finalizing sales. ",
+  },
+];
