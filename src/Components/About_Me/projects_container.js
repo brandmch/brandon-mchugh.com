0aa55d262dc7662c03ec41c46eb96fc5d7c899a3
brandmch <101467867+brandmch@users.noreply.github.com>
2022-05-21 13:40:10 -0500

    Added github link condition in

--- src/Components/About_Me/projects_container.js
+++ src/Components/About_Me/projects_container.js
@@ -1,16 +1,20 @@
 import React from "react";
 
 export default function Project_Container(props) {
-    // props: projectName, github, date, description, organization
-    return (
-        <div className="projectContainer">
-            <h1>{props.projectName}</h1>
-            <div className="info">
-                <a href={props.github} target="_blank" rel="noreferrer">GitHub</a>
-                <p>Organization: {props.organization}</p>
-                <p>{props.date}</p>
-                <p>{props.description}</p>
-            </div>
-        </div>
-    )
+  // props: projectName, github, date, description, organization
+  return (
+    <div className="projectContainer">
+      <h1>{props.projectName}</h1>
+      <div className="info">
+        {props.github && (
+          <a href={props.github} target="_blank" rel="noreferrer">
+            GitHub
+          </a>
+        )}
+        <p>Organization: {props.organization}</p>
+        <p>{props.date}</p>
+        <p>{props.description}</p>
+      </div>
+    </div>
+  );
 }
