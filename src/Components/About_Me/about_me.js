48bb0e3a9faadb4ffd63c27e2e9cac0d27d86f01
brandmch <101467867+brandmch@users.noreply.github.com>
2022-06-14 23:43:28 -0500

    buttons update begin

--- src/Components/About_Me/about_me.js
+++ src/Components/About_Me/about_me.js
@@ -3,56 +3,105 @@ import Education from "./education";
 import Projects from "./projects";
 import Skills from "./skills";
 import Work_Experience from "./work_experience";
-import MaintenanceAlert from '../../Maintence_Alert';
-import pics from '../../Images/images'
-
-
+import MaintenanceAlert from "../../Maintence_Alert";
+import pics from "../../Images/images";
 
 export default function About_Me() {
+  let [backgroundPic, setBackgroundPic] = React.useState([
+    "Images/pic",
+    1,
+    ".JPG",
+  ]);
 
-    let [backgroundPic, setBackgroundPic] = React.useState(["Images/pic", 1, ".JPG"])
+  const bgimg = {
+    backgroundImage: `URL(${backgroundPic.join("")})`,
+  };
 
-    const bgimg = {
-        backgroundImage: `URL(${backgroundPic.join("")})`
-
+  function nextPicNum() {
+    if (backgroundPic[1] === 15) {
+      setBackgroundPic(["Images/pic", 1, ".JPG"]);
+    } else {
+      setBackgroundPic((currentBackgroundPic) => [
+        "Images/pic",
+        currentBackgroundPic[1] + 1,
+        ".JPG",
+      ]);
     }
+  }
 
-    function nextPicNum() {
-        if (backgroundPic[1] === 15) {
-            setBackgroundPic(["Images/pic", 1, ".JPG"])
-        } else {
-            setBackgroundPic(currentBackgroundPic => ["Images/pic", currentBackgroundPic[1] + 1, ".JPG"])
-        }
-    }
-
-    return (
-
-        <div>
-            <header>
-                <div style={bgimg} className="container container_solid resume container_image" id="bg_img">
-                    <div className="title_wrapper">
-                        <div className="links">
-                            <a href="https://www.linkedin.com/in/mchugh-brandon/" target="_blank" rel="noreferrer">LinkedIn</a>
-                            <a href="https://github.com/brandmch" target="_blank" rel="noreferrer">GitHub</a>
-                            <a href="/meme_generator">Meme Generator</a>
-                            <a href="/home">Home</a>
-                        </div>
-                        <h1 id="h1">Brandon Lloyd McHugh</h1>
-                        <div className="button_arrow">
-                            <button type="button" onClick={nextPicNum}>Change Picture</button>
-                            {/* <img src={pics.downArrow} /> */}
-                        </div>
-                    </div>
-                </div>
-            </header>
-
-            <div className='section'>
-                <Skills />
-                <Projects />
-                <Work_Experience />
-                <Education />
+  return (
+    <div>
+      <header>
+        <div
+          style={bgimg}
+          className="container container_solid resume container_image"
+          id="bg_img"
+        >
+          <div className="title_wrapper">
+            <div className="links">
+              <button
+                type="button"
+                className="title_button"
+                onClick={(e) => {
+                  e.preventDefault();
+                  window.open(
+                    "https://www.linkedin.com/in/mchugh-brandon/",
+                    "_blank"
+                  );
+                }}
+              >
+                LinkedIn
+              </button>
+              <button
+                type="button"
+                className="title_button"
+                onClick={(e) => {
+                  e.preventDefault();
+                  window.open("https://github.com/brandmch", "_blank");
+                }}
+              >
+                GitHub
+              </button>
+              <button
+                className="title_button"
+                onClick={(e) => {
+                  e.preventDefault();
+                  window.open("/meme_generator", "_blank");
+                }}
+              >
+                Meme Generator
+              </button>
+              <button
+                className="title_button"
+                onClick={(e) => {
+                  e.preventDefault();
+                  window.open("/home", "_blank");
+                }}
+              >
+                Home
+              </button>
             </div>
-
+            <h1 id="h1">Brandon Lloyd McHugh</h1>
+            <div className="button_arrow">
+              <button
+                className="title_button"
+                type="button"
+                onClick={nextPicNum}
+              >
+                Change Picture
+              </button>
+              {/* <img src={pics.downArrow} /> */}
+            </div>
+          </div>
         </div>
-    )
+      </header>
+
+      <div className="section">
+        <Skills />
+        <Projects />
+        <Work_Experience />
+        <Education />
+      </div>
+    </div>
+  );
 }
--- src/Components/Meme_Generator/form.js
+++ src/Components/Meme_Generator/form.js
@@ -1,63 +1,67 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import data from "../../data/memedata"
+import data from "../../data/memedata";
 
 export default function Form() {
-    document.body.style.background = "wheat";
+  document.body.style.background = "wheat";
 
-    let allMemes = data.data.memes.map(meme => meme.url);
+  let allMemes = data.data.memes.map((meme) => meme.url);
 
-    const [meme, setMeme] = useState({
-        topText: "",
-        bottomText: "",
-        randomImage: "https://i.imgflip.com/1bgw.jpg"
-    });
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "https://i.imgflip.com/1bgw.jpg",
+  });
 
-    function getMemeImage() {
-        const randomNumber = Math.floor(Math.random() * allMemes.length);
-        const url = allMemes[randomNumber];
-        setMeme(prevMeme => ({
-            ...prevMeme,
-            randomImage: url
-        }));
-    }
+  function getMemeImage() {
+    const randomNumber = Math.floor(Math.random() * allMemes.length);
+    const url = allMemes[randomNumber];
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
+  }
 
-    function handleChange(event) {
-        const { name, value } = event.target;
-        setMeme(prevMeme => ({
-            ...prevMeme,
-            [name]: value
-        }));
-    }
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
+  }
 
-    return (
-        <div>
-            <div className="form_meme_generator">
-                <div className="input-fields_meme_generator">
-                    <input
-                        className="input1_meme_generator"
-                        type="text"
-                        placeholder="Top Text"
-                        onChange={handleChange}
-                        name="topText"
-                        value={meme.topText}
-                    />
-                    <input
-                        className="input2_meme_generator"
-                        type="text"
-                        placeholder="Bottom Text"
-                        onChange={handleChange}
-                        name="bottomText"
-                        value={meme.bottomText}
-                    />
-                </div>
-                <button onClick={getMemeImage}>Get a Random Meme!</button>
-            </div>
-            <div className="meme">
-                <img className="meme_image" src={meme.randomImage} />
-                <h2 className="meme_text top">{!meme.topText ? "Top Text" : meme.topText}</h2>
-                <h2 className="meme_text bottom">{!meme.bottomText ? "Bottom Text" : meme.bottomText}</h2>
-            </div>
+  return (
+    <div>
+      <div className="form_meme_generator">
+        <div className="input-fields_meme_generator">
+          <input
+            className="input1_meme_generator"
+            type="text"
+            placeholder="Top Text"
+            onChange={handleChange}
+            name="topText"
+            value={meme.topText}
+          />
+          <input
+            className="input2_meme_generator"
+            type="text"
+            placeholder="Bottom Text"
+            onChange={handleChange}
+            name="bottomText"
+            value={meme.bottomText}
+          />
         </div>
-    )
+        <button onClick={getMemeImage}>Get a Random Meme!</button>
+      </div>
+      <div className="meme">
+        <img className="meme_image" src={meme.randomImage} />
+        <h2 className="meme_text top">
+          {!meme.topText ? "Top Text" : meme.topText}
+        </h2>
+        <h2 className="meme_text bottom">
+          {!meme.bottomText ? "Bottom Text" : meme.bottomText}
+        </h2>
+      </div>
+    </div>
+  );
 }
